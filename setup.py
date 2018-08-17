99a47785dc2c6729d817494afa7fd6c9e905f889
SLPeoples <33819909+SLPeoples@users.noreply.github.com>
2018-08-17 16:49:58 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     # ready to develop your plugin, and add your own dependencies (if there are
     # any).
     install_requires=['qiime2 >= 2.0.0', 'pandas', 'scipy'],
-    dependency_links=['git+https://github.com/slpeoples/karenina.git#egg=0.0.0'],
+    dependency_links=['git+ssh://git@github.com/slpeoples/karenina.git@0.0.0#egg=karenina-0.0.0'],
 	author="Jesse Zaneveld",
     author_email="zaneveld@gmail.com",
     description="This script simulates microbiome " +
