4012b1f6991927bd15f9f98e1c2909ca510f2c65
SLPeoples <33819909+SLPeoples@users.noreply.github.com>
2018-08-17 15:27:24 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -18,8 +18,9 @@ setup(
     # visualizers provided as examples. Remove these dependencies when you're
     # ready to develop your plugin, and add your own dependencies (if there are
     # any).
-    install_requires=['qiime2 >= 2.0.0', 'pandas', 'scipy', 'git+https://github.com/slpeoples/karenina.git'],
-    author="Jesse Zaneveld",
+    install_requires=['qiime2 >= 2.0.0', 'pandas', 'scipy']
+    dependency_links = ['git+https://github.com/slpeoples/karenina.git']
+	author="Jesse Zaneveld",
     author_email="zaneveld@gmail.com",
     description="This script simulates microbiome " +
     "change over time using Ornstein-Uhlenbeck (OU) models.  These are " +
