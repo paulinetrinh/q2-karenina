4fc94ab3bbf8a1c25d14133437355a9f1ca2a392
SLPeoples <33819909+SLPeoples@users.noreply.github.com>
2018-08-18 15:17:04 -0700

    updates

--- q2_karenina/_spatial_ornstein_uhlenbeck.py
+++ q2_karenina/_spatial_ornstein_uhlenbeck.py
@@ -50,36 +50,27 @@ def spatial_ornstein_uhlenbeck(perturbation_fp:str, treatment_names:str, n_indiv
         individual_base_params,treatments,interindividual_variation, verbose=False)
     experiment.simulate_timesteps(0,n_timepoints, verbose=False)
     data, ids = experiment.q2_data()
-    _simulation_data(data, ids)
+    return _simulation_data(data, ids)
 	
 def _simulation_data(data, ids):
-    with open("ordination.txt","w") as outfile:
-        # Need to calculate eigenvalues
-        # outfile.write("Eigvals\t" + str(len(data)) + "\n\n")
-        outfile.write("Eigvals\t0" + "\n\n")
+    ordination = []
+	ordination.append("Eigvals\t0" + "\n\n")
+	ordination.append("Proportion explained\t0"+ "\n\n")
+	ordination.append("Species\t0\t0\n\n")
+	ordination.append("Site\t"+str(len(data)*len(data[0][0]))+"\t3\n")
+	dm = {}
+	j=0
+	for row in data:
+		identifier = ids[j]
+		for i in range(len(row[0])):
+			ordination.append(str(identifier)+"_t"+str(i)+"\t"+str(row[0][i])+"\t"+str(row[1][i])+"\t"+str(row[2][i])+"\n")
+			dm.update({str(identifier)+"."+str(i):[row[0][i],row[1][i],row[2][i]]})
+		j+=1
 
-        # Need to calculate propEx
-        # outfile.write("Proportion explained\t" + str(len(data)) + "\n\n")
-        outfile.write("Proportion explained\t0"+ "\n\n")
-
-        outfile.write("Species\t0\t0\n\n")
-        outfile.write("Site\t"+str(len(data)*len(data[0][0]))+"\t3\n")
-
-        # Need to separate pc1,2,3 and assign unique identifiers based on hash and timepoint.
-        dm = {}
-        j=0
-        for row in data:
-            identifier = ids[j]
-            for i in range(len(row[0])):
-                outfile.write(str(identifier)+"_t"+str(i)+"\t"+str(row[0][i])+"\t"+str(row[1][i])+"\t"+str(row[2][i])+"\n")
-                dm.update({str(identifier)+"."+str(i):[row[0][i],row[1][i],row[2][i]]})
-            j+=1
-
-        outfile.write("\n")
-        outfile.write("Biplot\t0\t0\n\n")
-        outfile.write("Site constraints\t0\t0\n")
-    outfile.close()
-
+    ordination.append("\n")
+    ordination.append("Biplot\t0\t0\n\n")
+    ordination.append("Site constraints\t0\t0\n")
+    
     # Distance matrix (euclidean)
     dm_0 = []
     dm_0.append("")
@@ -94,13 +85,6 @@ def _simulation_data(data, ids):
             dm_1.append(str(distance.euclidean(dm[key],dm[key1])))
         distance_matrix.append(dm_1)
 
-    with open("euclidean.txt","w") as outfile:
-        for row in distance_matrix:
-            for item in row:
-                outfile.write(str(item)+"\t")
-            outfile.write("\n")
-    outfile.close()
-
     #Mapping file
     md_0 = ["#SampleID","Subject","Treatment","Timepoint"]
     md_1 = ["#q2:types","categorical","categorical","numeric"]
@@ -111,14 +95,4 @@ def _simulation_data(data, ids):
     metadata = [md_0,md_1]
     for row in md:
         metadata.append(row)
-    with open("metadata.tsv","w") as outfile:
-        for row in metadata:
-            i=0
-            for item in row:
-                if i < len(row)-1:
-                    outfile.write(str(item)+"\t")
-                if i==len(row)-1:
-                    outfile.write(str(item))
-                i+=1
-            outfile.write("\n")
-    outfile.close()
+	return ordination, distance_matrix
