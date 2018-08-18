b60982fef1048080e0a75170107f0fd7b2fcdfa2
SLPeoples <33819909+SLPeoples@users.noreply.github.com>
2018-08-18 11:37:56 -0700

    update

--- q2_karenina/_spatial_ornstein_uhlenbeck.py
+++ q2_karenina/_spatial_ornstein_uhlenbeck.py
@@ -15,12 +15,13 @@ import pkg_resources
 import qiime2
 import q2templates
 from q2_types.ordination import PCoAResults
-
+from q2_types.ordination import PCoAResults
+from q2_types.distance_matrix import DistanceMatrix
 		
 def spatial_ornstein_uhlenbeck(perturbation_fp:str, treatment_names:str, n_individuals:str, 
 							n_timepoints:int, perturbation_timepoint:int,
 							perturbation_duration:int,interindividual_variation:float,
-							delta:float,lam:float,fixed_start_pos:str):
+							delta:float,lam:float,fixed_start_pos:str) -> PCoAResults, DistanceMatrix:
 	#pass items to k_OU, 
 	#ensure that metadata is saved with both PcOAResults, and DistanceMatrix!
 	pass
